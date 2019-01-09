3b36c41a0585acf686acd7e0e4658787c8850aa5
github username <github email address>
2019-01-09 14:07:09 -0500

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,37 @@
 // Code Keypad Component Here
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+class Keypad extends React.Component {
+    state = {
+      password: ""
+    };
+  
+    handleChange = event => {
+      console.log("gahchjsdv");
+      console.log(this.state.password);
+      console.log(event.target);
+      this.setState({ password: event.target.value });
+    };
+  
+    handleKeyUp = event => {
+      console.log("Entering password...");
+    };
+    render() {
+      return (
+        <form>
+          Password:{" "}
+          <input
+            onChange={this.handleChange}
+            onKeyUp={this.handleKeyUp}
+            name="password"
+            type="password"
+            value={this.state.password}
+          />
+        </form>
+      );
+    }
+  }
+  
+  export default Keypad;
+  
--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import ReactDOM from 'react-dom';
 import Keypad from './components/Keypad';
 import EyesOnMe from './components/EyesOnMe';
 
-ReactDOM.render(
-  <div>
-    <Keypad />
-    <EyesOnMe />
-  </div>,
-  document.getElementById('global')
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Keypad keypad={this.Keypad} />
+        <EyesOnMe eyesonme={this.EyesOnMe} />
+      </div>
+    );
+  }
+}
+
+
+
+
+const rootElement =document.getElementById('global')
+ReactDOM.render(<App />, rootElement);
+
